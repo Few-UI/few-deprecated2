1c337159a9dd3d5f0ee8d12127c76ea9de9ce3b4
jesse23 <pengwenjia@gmail.com>
2020-05-13 14:55:14 +0000

    1st cut of vue render function

--- src/FirstVue.tsx
+++ src/FirstVue.tsx
@@ -1,11 +1,20 @@
+// https://vuejsdevelopers.com/2020/03/16/vue-js-tutorial/
+// https://github.com/vuejs/rfcs/blob/master/active-rfcs/0008-render-function-api-change.md
+
 import Vue from 'vue';
 
+const h = Vue.h;
+
 const FirstComponent = {
-    template: '<div>Hello Vue!</div>'
+    //template: '<div>Hello Vue!</div>'
+    name: 'test',
+    render() {
+        return <div>Hello Vue</div>;
+    }
 };
 
 const App = {
-    template: '<button @click="inc">Clicked {{ count }} times.</button>',
+    template: '<first-component />',
     setup() {
     const count = Vue.ref( 0 );
     const inc = () => {
@@ -17,7 +26,7 @@ const App = {
     };
     },
     components: {
-        aaa: null
+        'first-component': FirstComponent
     },
     name: 'app'
 };
