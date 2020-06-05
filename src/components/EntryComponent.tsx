f31d878e62582010117d3404050d7d1e7752fdda
jesse23 <pengwenjia@gmail.com>
2020-06-05 21:55:31 +0000

    refactor(jsx): jsx approach with component
    try to use jsx example with few component

--- src/components/EntryComponent.tsx
+++ src/components/EntryComponent.tsx
@@ -8,15 +8,25 @@ export default {
     name: 'EntryComponent',
     // h( 'div', null, 'current number: ', model.name ) is working too, but
     // it is not friendly for jsbeautify
+    // https://www.typescriptlang.org/docs/handbook/jsx.html#type-checking
     view: h => (): JSX.Element =>
-        h( 'div', {},
-            h( ViewExample ),
-            h( PropExample, {
-                firstName: 'Final',
-                lastName: 'Fantasy'
-            } )
-            // h( DispatchInitExample )
-        ),
+        <div>
+            {/*
+            // @ts-ignore */}
+            <ViewExample />
+            {/*
+            // @ts-ignore */}
+            <PropExample firstName='Final' lastName='Fantasy' />
+        </div>,
     init: () => ( {} )
 } as ComponentDef;
 
+/*
+const h = '3';
+const Test = ( props: object ) => ( {
+    type: 'button',
+    props: null as object,
+    key: 3
+} );
+<Test />;
+*/
