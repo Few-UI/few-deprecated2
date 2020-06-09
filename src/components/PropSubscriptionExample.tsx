55be90593a18f401a0372454b3a521187b180ae1
jesse23 <pengwenjia@gmail.com>
2020-06-09 21:10:18 +0000

    refactor(prop): add comments
    keep the null component part as comments

--- src/components/PropSubscriptionExample.tsx
+++ src/components/PropSubscriptionExample.tsx
@@ -7,6 +7,9 @@ const PropSubscriptionWidget = {
         <div>
             <div>Name: {firstName || 'Jane'} {lastName || 'Dole'}</div>
             <div>Address: {model.address}</div>
+            {/*
+                <div>{( actions.getAddress(), '' )}</div>
+            */}
         </div>,
     init: () => ( {
         address: 'loading...'
