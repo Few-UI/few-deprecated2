b7e58e8bcd6f121bae20f8bde6ea093edc3af1da
jesse23 <pengwenjia@gmail.com>
2020-06-19 03:57:31 +0000

    refactor(type): cleanup
    clean up type def

--- src/components/JsxTypeExample.tsx
+++ src/components/JsxTypeExample.tsx
@@ -11,15 +11,18 @@
  * - Element has conflict with ElementClass factory function practice, that is why
  *   vue set Element as {}
  */
+import { createElement } from 'react';
+import { ComponentDef } from '../types';
 namespace Few {
     // eslint-disable-next-line @typescript-eslint/no-empty-interface
     export interface Element {
         // (): number;
+        // (): JSX.Element;
     }
 
     export interface ElementClass {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        render: any;
+        render: () => JSX.Element;
     }
 }
 
@@ -27,32 +30,12 @@ export type { Few as JSX };
 
 /**
  * dummy h function
+ * @param type component definition
  * @returns anything
  */
-export function h( /*...*/ ): Few.Element {
-    return {
-        a: 5
-    };
-}
-
-/**
- * Class following Element Class Pattern
- * Note: object doesn't work
- */
-class MyComponent {
-    render: () => 3
-}
-
-/**
- * Factory function example
- * @returns object in ElementClass pattern
- */
-function MyFactoryFunction(): Few.ElementClass {
-    return {
-        render: (): number => {
-            return 3;
-        }
-    };
+export function h( type: ComponentDef ): JSX.Element {
+    type;
+    return createElement( 'div' );
 }
 
 // https://github.com/microsoft/TypeScript/issues/23762
@@ -63,14 +46,34 @@ export namespace h {
     export import JSX = Few;
 }
 
-const FunctionComponent = (): Few.Element => ( { a: 5 } );
+const FunctionComponent = ( { value }: { value: number } ): Few.Element => ( { a: value } );
 
-const Function2 = (): number => 5;
+const Function2 = ( { value }: { value: number } ): number => value + 1;
 
-const Example = (): Few.Element =>
+const RenderFn = ( { name }: { name: string } ): JSX.Element => createElement( 'div', null, name );
+
+// Class following Element Class Pattern. Object doesn't work
+class MyComponent {
+    render(): JSX.Element {
+        this;
+        return createElement( 'div' );
+    }
+}
+
+// Factory function example
+const MyFactoryFunction = (): Few.ElementClass => ( {
+    render: (): JSX.Element => {
+        return createElement( 'div' );
+    }
+} );
+
+type TestElem = Few.Element | JSX.Element;
+
+const Example = (): TestElem =>
     <>
-        <FunctionComponent />
-        <Function2 />
+        <FunctionComponent value={3} />
+        <RenderFn name='5' />
+        <Function2 value={5} />
         <MyComponent />
         <MyFactoryFunction />
     </>;
