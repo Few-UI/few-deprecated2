1b3462712affd9a9def5b2d8bfe2f765cf593489
jesse23 <pengwenjia@gmail.com>
2020-07-11 19:10:24 +0000

    refactor(form): update form case
    update form practice with proper interface

--- src/components/FormExample.tsx
+++ src/components/FormExample.tsx
@@ -3,7 +3,10 @@ import { defineComponent, getFormInput } from '@/utils';
 const Form = defineComponent( {
     name: 'Form',
     view: h => ( { props } ): JSX.Element =>
-        <form onSubmit={props.onSubmit}>
+        <form onSubmit={ ( e: any ) => {
+                e.preventDefault();
+                props.onSubmit( getFormInput( e.target ) );
+            }}>
             <input name='name'></input>
             <button type='submit'>submit</button>
         </form>
@@ -18,9 +21,11 @@ export default defineComponent( {
             <div>Result: {model.name}</div>
         </>,
     actions: {
-        updateResult: ( { dispatch }, e ): void => {
-            e.preventDefault();
-            dispatch( { path: 'name', value: getFormInput( e.target ).name } );
+        updateResult: ( { dispatch }, values ): void => {
+            dispatch( {
+                path: 'name',
+                value: values.name
+            } );
         }
     }
 } );
