ed95a71ceef25d154db92db72fb76e530ae529e0
jesse23 <pengwenjia@gmail.com>
2020-07-11 21:08:30 +0000

    feat(form): enhance form example
    enhance form example with more property

--- src/components/FormExample.tsx
+++ src/components/FormExample.tsx
@@ -1,31 +1,49 @@
 import { defineComponent, getFormInput } from '@/utils';
+import { FormEvent } from 'react';
 
-const Form = defineComponent( {
-    name: 'Form',
+const TestForm = defineComponent( {
+    name: 'TestForm',
     view: h => ( { props } ): JSX.Element =>
-        <form onSubmit={ ( e: any ) => {
+        <form onSubmit={ ( e: FormEvent ): void => {
+                // if put it here, means we want to wrap event
                 e.preventDefault();
-                props.onSubmit( getFormInput( e.target ) );
+                props.action( getFormInput( e.target as Element ) );
             }}>
-            <input name='name'></input>
+            {
+                Object.entries( props.fields ).map( ( [ k, v ] ) =>
+                    <div  key={k}>
+                        <label htmlFor={k}>{( v as any ).name}:</label>
+                        <input type={( v as any ).type} id={k} name={k}></input>
+                    </div>
+                )
+            }
             <button type='submit'>submit</button>
         </form>
-
 } );
 
 export default defineComponent( {
     name: 'FormExample',
     view: h => ( { model, actions } ): JSX.Element =>
         <>
-            <Form onSubmit={actions.updateResult}></Form>
-            <div>Result: {model.name}</div>
+            <TestForm action={actions.updateResult} fields={model.fields}></TestForm>
+            <pre>Result: {model.result}</pre>
         </>,
-    actions: {
-        updateResult: ( { dispatch }, values ): void => {
-            dispatch( {
-                path: 'name',
-                value: values.name
-            } );
+    init: () => ( {
+        fields: {
+            name: {
+                name: 'name',
+                type: 'text'
+            },
+            desc: {
+                name: 'desc',
+                type: 'text'
+            }
         }
+    } ),
+    actions: {
+        updateResult: ( { dispatch }, formValues ): void => dispatch( {
+            path: 'result',
+            value: JSON.stringify( formValues, null, 2 )
+        } )
     }
 } );
